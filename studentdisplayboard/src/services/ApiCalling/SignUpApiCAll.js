7945ecfee8724da6c9491f7c5a156e1b4335c7a8
ri-kumar <ritesh.kumar@trafilea.com>
2023-03-22 15:02:04 +0530

    add deployed website url

--- studentdisplayboard/src/services/ApiCalling/SignUpApiCAll.js
+++ studentdisplayboard/src/services/ApiCalling/SignUpApiCAll.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const signUpUser = async (user) => {
   const response = await axios.post(
-    "http://localhost:8000/api/v1/signup",
+    "https://dashboard-tdoy.onrender.com/api/v1/signup",
     user
   );
   return response;
--- studentdisplayboard/src/services/ApiCalling/loginApiCall.js
+++ studentdisplayboard/src/services/ApiCalling/loginApiCall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchToken = async (user) => {
-  const response = await axios.post("http://localhost:8000/api/v1/login", user);
+  const response = await axios.post("https://dashboard-tdoy.onrender.com/api/v1/login", user);
   if(response.data.token){
     localStorage.setItem('token',response.data.token);
   }
